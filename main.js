ae743c225100d3733dd3093262b76e2e3237ddba
heavyn1 <bunedusupov968@gmail.com>
2024-05-09 14:51:50 +0500

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 for (let i = 0; i < array.length; i++) {
-    let check = array[i] % 2;
+    
+    let check = array[i] % 2;   
+
     if (check === 0) {
         console.log(array[i]);
     } else {
